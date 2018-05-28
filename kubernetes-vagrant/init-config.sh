bacb7dbefd6c20fcde456f29a6d3f10fbe789078
screwyterror83 <screwyterror83@gmail.com>
2018-05-27 22:53:45 -0700

    Update init-config.sh

--- kubernetes-vagrant/init-config.sh
+++ kubernetes-vagrant/init-config.sh
@@ -21,19 +21,19 @@ apt-get remove -y docker docker-engine docker.io > /dev/null 2>&1
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common > /dev/null 2>&1
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 apt-key fingerprint 0EBFCD88 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null 2>&1
 apt-get update -y > /dev/null 2>&1
 apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial > /dev/null 2>&1
 
 #Install 'kubeadm' 'kubelet' and 'kubectl'
 echo "Install 'kubeadm' 'kubelet' and 'kubectl'"
 apt-get update > /dev/null 2>&1
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
 
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
+apt-get update -y > /dev/null 2>&1
 
 apt-get install -y kubelet kubeadm kubectl > /dev/null 2>&1
 
